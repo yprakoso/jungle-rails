2ef7e9d0d1b719dbbe800e027557808bc1e5cb09
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-02-19 20:48:12 +0000

    add http basic auth for admin and fix empty cart

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
   def index
     @categories = Category.all
   end
