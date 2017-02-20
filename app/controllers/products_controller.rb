c92848aa4ec8bb42bfd374faa460b1170ae8f371
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-02-20 23:54:15 +0000

    finish product rating except styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,8 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
-    @reviews = Review.where(product_id: params[:id])
+    @reviews = Review.where(product_id: params[:id]).order(created_at: :desc)
+    @review = @product.reviews.new
   end
 
 end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,31 @@
+class ReviewsController < ApplicationController
+
+  before_filter :authorize
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    review_i_want_to_destroy = Review.find(params[:id])
+    review_owner = review_i_want_to_destroy.user_id
+    if review_owner == current_user.id
+      review_i_want_to_destroy.destroy
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
+end
